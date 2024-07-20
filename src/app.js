189989aca74719782bb683803e6a2fa3fc7945b5
Thomas Sheehan <tsheehan@indeed.com>
2024-07-20 15:23:18 +0900

    Static data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const pastes = require("./data/pastes-data");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+// Application-level middleware
+app.use(morgan("dev"));
+
+// Route functions
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${PasteId}`);
+  }
+});
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
 
 // Not found handler
 app.use((request, response, next) => {
