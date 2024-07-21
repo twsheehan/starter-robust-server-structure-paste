ead8fa92edabf986c472413b25bb5bcb84ea1a86
Thomas Sheehan <tsheehan@indeed.com>
2024-07-21 14:32:25 +0900

    Restful APIs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const app = express();
 const pastes = require("./data/pastes-data");
 
+// Add a `body` property to the request
+app.use(express.json());
+
 // Application-level middleware
 app.use(morgan("dev"));
 
@@ -14,12 +17,35 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${PasteId}`);
+    next(`Paste id not found: ${pasteId}`);
   }
 });
 
-app.use("/pastes", (req, res) => {
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text } = {} } = req.body;
+
+  if (text) {
+    const newPaste = {
+      id: ++lastPasteId, //Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // Not found handler
